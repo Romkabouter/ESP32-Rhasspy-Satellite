d6b6c77f95c13b6f5619f63bc26221f4b25f3e23
Markus Ressel <markus9100@gmail.com>
2021-03-18 16:07:47 +0100

    Added "sudo" to voice_esptool command

--- PlatformIO/deploy.sh
+++ PlatformIO/deploy.sh
@@ -23,7 +23,7 @@ else
     cp .pio/build/esp32dev/partitions.bin .
     echo "Loading firmware: $FIRMWARE to $1"
     echo ""
-    tar cf - *.bin | ssh pi@$1 'tar xf - -C /tmp;sudo voice_esp32_reset;voice_esptool --chip esp32 --port /dev/ttyS0 --baud 1500000 --before default_reset --after hard_reset write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /tmp/bootloader.bin 0x10000 /tmp/firmware.bin 0x8000 /tmp/partitions.bin'
+    tar cf - *.bin | ssh pi@$1 'tar xf - -C /tmp;sudo voice_esp32_reset;sudo voice_esptool --chip esp32 --port /dev/ttyS0 --baud 1500000 --before default_reset --after hard_reset write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /tmp/bootloader.bin 0x10000 /tmp/firmware.bin 0x8000 /tmp/partitions.bin'
     echo "done"
     echo ""
     echo "[SUCCESS] Please disconnect your MatrixVoice from the RaspberryPi and reconnect it alone for future OTA updates."
